5191789afabd5c89c8bbc930a222243db454f31d
Bhavya-1245 <gargbhavya104@gmail.com>
2025-06-15 21:44:36 +0530

    Time: 48 ms (57.53%) | Memory: 73.7 MB (35.00%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -7,15 +7,13 @@ class Solution {
         ArrayList<int[]> arr=new ArrayList<>();
         arr.add(intervals[0]);
         int val=intervals[0][1];
-        int count=0;
         for(int i=0; i<n-1; i++){
             if(val<=intervals[i+1][0]){
+                arr.add(intervals[i+1]);
                 val=intervals[i+1][1];
-            }else{
-                count++;
             }
         }
 
-        return count;
+        return n-arr.size();
     }
 }
