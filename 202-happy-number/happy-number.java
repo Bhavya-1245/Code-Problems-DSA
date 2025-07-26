15e599f63565cae0a95302a7396514667de77f4b
Bhavya-1245 <gargbhavya104@gmail.com>
2025-07-26 17:21:43 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (85.59%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isHappy(int n) {
+        
+        if(n == 1 || n == 7) return true;
+        
+        
+
+        while(n>9){
+            int num = n;
+            int sum = 0;
+            while(num>0){
+                sum+=Math.pow(num%10,2);
+                num/=10;
+            }
+            if(sum == 1 || sum == 7) return true;
+            n = sum;
+        }
+
+        return false;
+    }
+}
