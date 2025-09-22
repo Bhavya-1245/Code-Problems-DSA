49c294589c47809250cce130b0d790b005562a42
Bhavya-1245 <gargbhavya104@gmail.com>
2025-09-22 22:35:13 +0530

    Time: 13 ms (41.77%) | Memory: 45.1 MB (28.86%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s=="" || s.length()==1) return true;
+       String str = s.toLowerCase().replaceAll("[^a-zA-Z0-9]","");
+
+       int i = 0;
+       int j = str.length()-1;
+
+       while(i<j){
+            if(str.charAt(i)!=str.charAt(j)) return false;
+            i++;
+            j--;
+       } 
+
+       return true;
+    }
+}
