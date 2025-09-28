2c3907bd3379d2a2704595e3ff8e1adc71b17296
Bhavya-1245 <gargbhavya104@gmail.com>
2025-09-28 23:01:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (34.80%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,26 +1,27 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         
-        int []res = new int[nums1.length];
-        Arrays.fill(res, -1);
-        int []ans = new int[nums2.length];
-
-        Stack<Integer> st = new Stack<>();
-
-        for(int i=0; i<nums2.length; i++){
-            while(!st.isEmpty() && nums2[st.peek()]<nums2[i]){
-                ans[st.peek()] = nums2[i];
-                st.pop();
-            }
-            st.push(i);
+        int []idx = new int[11111];
+        
+        for(int i=0;i<nums2.length;i++){
+            idx[nums2[i]] = i;
         }
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int i:nums2) arr.add(i);
+        
+
         for(int i=0; i<nums1.length; i++){
-            if(ans[arr.indexOf(nums1[i])]==0) res[i] = -1;
-            else res[i] = ans[arr.indexOf(nums1[i])];
+            nums1[i] = greatest(nums2, idx[nums1[i]]);
         }
+        
 
-        return res;
+        return nums1;
+    }
+
+    public static int greatest(int []nums2, int idx){
+        int m = nums2.length;
+
+        for(int i=idx+1;i<m;i++){
+            if(nums2[i]>nums2[idx]) return nums2[i];
+        }
+        return -1;
     }
 }
