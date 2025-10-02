91df6e5e81a3fde30bb34bb5b27e7d5154798ea2
Bhavya-1245 <gargbhavya104@gmail.com>
2025-10-03 02:37:21 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (78.98%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+
+        for(int i=0; i<32; i++){
+          int bit = (n&1);
+
+          result |= (long)bit << (31-i);
+
+          n >>>=1;
+        }
+
+        return (int) result;
+    }
+}
