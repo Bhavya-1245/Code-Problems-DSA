408fbfb9f76970592e1094f4986300b234f8c4d2
Bhavya-1245 <gargbhavya104@gmail.com>
2025-12-04 10:58:23 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (24.28%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,10 +1,10 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n==0) return 1;
-        if(n<0) return 0;
-        int []dp = new int[n+1];
-        dp[0] = 1;
-        return total_chance(n, dp);
+        if(n==1) return 1;
+        if(n<=0) return 0;
+        // int []dp = new int[n+1];
+        // dp[0] = 1;
+        return total_chance(n);
         
     }
 
@@ -28,12 +28,26 @@ class Solution {
     // }
 
     // bottom up approach
-    public static int total_chance(int n, int[] dp){
-      dp[1] = 1;
-      for(int i=2; i<=n; i++){
-        dp[i] = dp[i-1] + dp[i-2];
+    // public static int total_chance(int n, int[] dp){
+    //   dp[1] = 1;
+    //   for(int i=2; i<=n; i++){
+    //     dp[i] = dp[i-1] + dp[i-2];
 
-      }
-      return dp[n];
+    //   }
+    //   return dp[n];
+    // }
+
+    // space optimized
+    public static int total_chance(int n){
+        int step1 = 1;
+        int step2 = 1;
+        int ans = 0;
+        for(int i=2; i<=n;i++){
+          ans = step1 + step2;
+          step2 = step1;
+          step1 = ans;
+        }
+
+        return ans;
     }
 }
