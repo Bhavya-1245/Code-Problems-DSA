799263d042ac2e54181649633155d51ee4d18af2
Bhavya-1245 <gargbhavya104@gmail.com>
2025-12-09 09:29:30 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (30.20%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public static int solve(int []nums, int i, int n, int []dp){
+      if(i>=n) return 0;
+      if(dp[i]!=-1) return dp[i];
+
+      int select1 = nums[i] + solve(nums, i+2, n, dp);
+      int select2 = nums[i] + solve(nums, i+3, n, dp);
+      int skip = solve(nums, i+1, n, dp);
+
+      return dp[i] = Math.max(select1, Math.max(select2, skip));
+    }
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int []dp = new int[101];
+        Arrays.fill(dp, -1);
+        return solve(nums, 0, n, dp);
+    }
+}
