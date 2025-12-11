b78e112d5487c605d6e0208ff63f0a5e7c1cfcb4
Bhavya-1245 <gargbhavya104@gmail.com>
2025-12-11 10:51:52 +0530

    Time: 1 ms (99.81%) | Memory: 45.4 MB (58.88%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,22 +1,23 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        for(int i=n-2; i>=0; i--){
-            List<Integer> current = triangle.get(i);
-            List<Integer> next = triangle.get(i+1);
 
-            for(int j=0; j<current.size(); j++){
-                int path1 = next.get(j);
-                int path2 = next.get(j+1);
+    public static int minimumSum(List<List<Integer>> triangle, int i, int j, int n, int [][]dp){
+      if(i==n-1){
+        return triangle.get(i).get(j);
+      }
 
-                int minPath = Math.min(path1, path2);
-                int total = current.get(j) + minPath;
-                current.set(j, total);
-            }
-        }
+      if(dp[i][j] != Integer.MIN_VALUE) return dp[i][j];
 
-        return triangle.get(0).get(0);
+      int sum1 = minimumSum(triangle, i+1, j, n, dp);
+      int sum2 = minimumSum(triangle, i+1, j+1, n, dp);
+
+      return dp[i][j] = triangle.get(i).get(j) + Math.min(sum1, sum2);
     }
 
-    
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int [][]dp = new int[n][n];
+        for(int []row: dp) Arrays.fill(row, Integer.MIN_VALUE);
+        return minimumSum(triangle, 0, 0, n, dp);
+    }
+
 }
