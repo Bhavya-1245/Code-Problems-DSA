f3da2a7941116a3ef4be79ccc49fb3db183b482c
Bhavya-1245 <gargbhavya104@gmail.com>
2025-12-15 23:43:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -1,27 +1,45 @@
 class Solution {
     
-     static int [][]dp;
+     static boolean [][]dp;
     
-    static int helper(int []arr, int i, int sum){
-        if(sum == 0) return 1;
-        if(i == arr.length){
-            if(sum==0) return 1;
-            return 0;
-        }
-        if(sum<0) return 0;
-        if(dp[i][sum]!=-1) return dp[i][sum];
-        int pick = helper(arr, i+1, sum - arr[i]);
-        int skip = helper(arr, i+1, sum);
-        return dp[i][sum] = (pick> 0 || skip > 0) ? 1 : 0;
-    }
+    // static int helper(int []arr, int i, int sum){
+    //     if(sum == 0) return 1;
+        
+    //     if(i == arr.length){
+    //         if(sum==0) return 1;
+    //         return 0;
+    //     }
+        
+    //     if(sum<0) return 0;
+        
+    //     if(dp[i][sum]!=-1) return dp[i][sum];
+        
+    //     int pick = helper(arr, i+1, sum - arr[i]);
+        
+    //     int skip = helper(arr, i+1, sum);
+        
+    //     return dp[i][sum] = (pick> 0 || skip > 0) ? 1 : 0;
+    // }
     
     static Boolean isSubsetSum(int arr[], int sum) {
         // code here
-        dp = new int[arr.length][sum+1];
+        dp = new boolean[arr.length+1][sum+1];
+        int n = arr.length;
+        for(int i = 0; i<=n; i++){
+            dp[i][0] = true;
+        }
         
-        for(int []row: dp) Arrays.fill(row, -1);
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=sum; j++){
+                if(arr[i-1]<=j){
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j - arr[i-1]];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
         
         // do[0] = sum; 
-        return helper(arr,0, sum) == 1;
+        return dp[n][sum];
     }
 }
