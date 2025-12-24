dbf50c829ab3685d44faa966c9163039d62553bb
Bhavya-1245 <gargbhavya104@gmail.com>
2025-12-24 09:49:51 +0530

    Time: 18 ms (91.18%) | Memory: 48.2 MB (8.53%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,22 +1,22 @@
 class Solution {
-    static int n = 0, m = 0;
+    static int n = 0;
     static int [][]dp;
-    public static int helper(String s1, String s2, int i, int j){
-      if(i>=n || j>=m) return 0;
+    public static int helper(String s1, int i, int j){
+      if(i>=j) return 0;
       if(dp[i][j]!=-1) return dp[i][j];
-      if(s1.charAt(i)==s2.charAt(j)){
-        return dp[i][j] = 1 + helper(s1, s2, i+1, j+1);
+      if(s1.charAt(i)==s1.charAt(j)){
+        return dp[i][j] = helper(s1, i+1, j-1);
       }
 
-      return dp[i][j] = Math.max(helper(s1, s2, i+1, j), helper(s1, s2, i, j+1));
+      return dp[i][j] = 1 + Math.min(helper(s1, i+1, j), helper(s1, i, j-1));
     }
     public int minInsertions(String s) {
         String s1 = s;
         String s2 = new StringBuilder(s).reverse().toString();
 
-        n = s1.length(); m = s2.length();
-        dp = new int[n][m];
+        n = s1.length(); 
+        dp = new int[n][n];
         for(int []row: dp) Arrays.fill(row, -1);
-        return s1.length() - helper(s1, s2, 0, 0); 
+        return helper(s1, 0, n-1); 
     }
 }
