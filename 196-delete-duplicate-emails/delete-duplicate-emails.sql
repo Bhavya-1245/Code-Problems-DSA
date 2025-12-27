afce62f21de81d9aba2d9f2e19252b6188213327
Bhavya-1245 <gargbhavya104@gmail.com>
2025-12-27 23:43:31 +0530

    Time: 1996 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,5 +1,10 @@
 # Write your MySQL query statement below
-DELETE p1 from person p1, person p2
-where p1.email = p2.email and p1.id>p2.id;
+DELETE p1 FROM Person p1
+JOIN Person p2
+ON p2.email = p1.email
+AND p1.id>p2.id;
 
+
+-- DELETE p1 from person p1, person p2
+-- where p1.email = p2.email and p1.id>p2.id;
 -- Select id, email from person
