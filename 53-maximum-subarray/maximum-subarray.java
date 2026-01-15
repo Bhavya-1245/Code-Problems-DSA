8f7891f9750e28d1fa88220143101d986a3e21e7
Bhavya-1245 <gargbhavya104@gmail.com>
2026-01-15 23:42:20 +0530

    Time: 1 ms (99.65%) | Memory: 81.1 MB (6.12%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -2,21 +2,34 @@ class Solution {
   static int n;
   static int []dp;
 
-  static int solve(int []nums, int i){
-    if(i==0) return nums[i];
-    if(dp[i]!=-1) return dp[i];
+  // static int solve(int []nums, int i){
+  //   if(i==0) return nums[i];
+  //   if(dp[i]!=-1) return dp[i];
 
-    return dp[i] = Math.max(nums[i], nums[i]+solve(nums,i-1));
-  }
-    public int maxSubArray(int[] nums) {
-        n = nums.length;
-        dp = new int[n];
-        Arrays.fill(dp, -1);
-        int ans = nums[0];
-        for(int i=1;i<n;i++) ans = Math.max(ans,solve(nums, i));
+  //   return dp[i] = Math.max(nums[i], nums[i]+solve(nums,i-1));
+  // }
+  //   public int maxSubArray(int[] nums) {
+  //       n = nums.length;
+  //       dp = new int[n];
+  //       Arrays.fill(dp, -1);
+  //       int ans = nums[0];
+  //       for(int i=1;i<n;i++) ans = Math.max(ans,solve(nums, i));
         
         
-        return ans;
+  //       return ans;
 
-    }
+  //   }
+
+        public int maxSubArray(int []nums){
+          n = nums.length;
+          dp = new int[n];
+          dp[0] = nums[0];
+          int ans = nums[0];
+          for(int i=1;i<n;i++){
+            dp[i] = Math.max(nums[i], nums[i]+dp[i-1]);
+            ans = Math.max(ans, dp[i]);
+          }
+
+          return ans;
+        }
 }
