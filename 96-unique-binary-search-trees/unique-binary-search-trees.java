8e62dad79c193a7f57345c7805d9ba8f6c7de4ac
Bhavya-1245 <gargbhavya104@gmail.com>
2026-01-17 12:52:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (35.04%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,12 +1,23 @@
 class Solution {
-    public int numTrees(int n) {
+    static int []dp;
+    public static int solve(int n){
         if(n<=1) return 1;
-
-        int ans = 0;
+        if(dp[n]!=0) return dp[n];
+        int count = 0;
         for(int i=1;i<=n;i++){
-          ans+=numTrees(i-1)*numTrees(n-i);
+          count += solve(i-1)*solve(n-i);
         }
 
-        return ans;
+        return dp[n]= count;
+    }
+    public int numTrees(int n) {
+        // if(dp[n]!=0) return dp[n];
+        dp = new int[n+1];
+        Arrays.fill(dp,0);
+        dp[0] = 1;
+
+        
+
+        return solve(n);
     }
 }
