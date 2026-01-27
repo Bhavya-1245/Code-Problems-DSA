9d1be3c98027f0a34725a4001570802c1a3706cb
Bhavya-1245 <gargbhavya104@gmail.com>
2026-01-27 23:10:00 +0530

    Time: 12 ms (92.81%) | Memory: 46.1 MB (94.26%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,30 +1,38 @@
 class Solution {
     static int n = 0;
-    static int [][]dp;
+    static int []dp;
 
-    public static int solve(int []coins, int i, int amt){
-      if(amt == 0) return 0;
-      if(amt<0) return 100000;
-      if(i<0) return 100000;
-      if(dp[i][amt]!=-1) return dp[i][amt];
-      int take = 1 + solve(coins, i, amt - coins[i]);
+    // public static int solve(int []coins, int i, int amt){
+    //   if(amt == 0) return 0;
+    //   if(amt<0) return 100000;
+    //   if(i<0) return 100000;
+    //   if(dp[i][amt]!=-1) return dp[i][amt];
+    //   int take = 1 + solve(coins, i, amt - coins[i]);
 
       
 
-      int skip = solve(coins, i - 1, amt);
+    //   int skip = solve(coins, i - 1, amt);
 
-      return dp[i][amt] = Math.min(take, skip);
-    }
+    //   return dp[i][amt] = Math.min(take, skip);
+    // }
 
     public int coinChange(int[] coins, int amount) {
         n = coins.length;
-        dp = new int[n][amount+1];
-
+        dp = new int[amount+1];
         
-        for(int []col: dp) Arrays.fill(col, -1);
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(int c:coins){
+          for(int sum = c; sum<=amount; sum++){
+              if(dp[sum-c]==Integer.MAX_VALUE) continue;
+              dp[sum] = Math.min(dp[sum], 1 +dp[sum - c]);
+          }
+        }
         
-        int ans = solve(coins, n-1, amount);
+        // for(int []col: dp) Arrays.fill(col, -1);
+        
+        // int ans = solve(coins, n-1, amount);
 
-        return ans == 100000 ? -1 : ans;
+        return dp[amount]>=Integer.MAX_VALUE ? -1 : dp[amount];
     }
 }
