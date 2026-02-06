c6427d1459c1131a22fbceb9a560f34809673222
Bhavya-1245 <gargbhavya104@gmail.com>
2026-02-06 10:36:11 +0530

    Time: 6 ms (98.83%) | Memory: 60.1 MB (55.27%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -0,0 +1,36 @@
+class Solution {
+    static int n, m;
+
+    static void dfs(int [][]board, int i, int j){
+      if(i<0 || i>=n || j>=m || j<0 || board[i][j]!=1) return;
+      
+      board[i][j] = 0;
+
+      dfs(board, i+1, j); // down
+      dfs(board, i-1, j); // up
+      dfs(board, i, j-1); // left
+      dfs(board, i, j+1); // right
+    }
+    public int numEnclaves(int[][] board) {
+        n = board.length; m = board[0].length;
+
+        for(int i=0; i<n;i++){
+          dfs(board, i, 0);
+          dfs(board, i, m-1);
+        }
+        for(int i=0; i<m;i++){
+          dfs(board, 0, i);
+          dfs(board, n-1, i);
+        }
+        int result = 0;
+        for(int i=0;i<n;i++){
+          for(int j=0;j<m;j++){
+            if(board[i][j] == 1){
+              result++;
+            }
+          }
+        }
+
+        return result;
+    }
+}
