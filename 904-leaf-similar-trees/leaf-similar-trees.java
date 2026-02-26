8077a3dd2514a90b9e40cc3d6ca0026ecc52c2dc
Bhavya-1245 <gargbhavya104@gmail.com>
2026-02-26 16:07:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (69.67%) - LeetSync

---
+++ 904-leaf-similar-trees/leaf-similar-trees.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public static void solve(TreeNode root, ArrayList<Integer> list){
+      if(root==null) return;
+
+      if(root.left == null && root.right == null){
+        list.add(root.val);
+        return;
+      }
+
+      solve(root.left, list);
+      solve(root.right, list);
+    }
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+
+        solve(root1, list1);
+        solve(root2, list2);
+
+        return list1.equals(list2);
+    }
+}
